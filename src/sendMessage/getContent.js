5fac5af697f3c0021043fc76ba9cf77b06470f98
wang <jinitt@qq.com>
2022-11-03 00:54:54 +0800

    style: format and init

--- src/sendMessage/getContent.js
+++ src/sendMessage/getContent.js
@@ -1,12 +1,12 @@
 var axios = require("axios");
-const { CHP_URL } = require('../config/config');
+const { CHP_URL } = require("../config/config");
 const getContent = async () => {
-  let { data } = await axios({
-    url: CHP_URL,
-  });
-  return data;
+    let { data } = await axios({
+        url: CHP_URL,
+    });
+    return data;
 };
 
 module.exports = {
-  getContent,
+    getContent,
 };
